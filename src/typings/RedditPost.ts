f104d31c452a517fb9cff2946f0e8a28aa9eb645
Jim Ke <jimke2000@gmail.com>
2021-11-10 15:39:54 +1100

    feat(types): added awards

--- src/typings/RedditPost.ts
+++ src/typings/RedditPost.ts
@@ -7,9 +7,19 @@ export interface RedditPost {
   upvoteRatio: number;
   thumbnail: RedditURL;
   imageURL: RedditURL;
-  title: string,
-  edited: boolean,
-  spoiler: boolean,
-  NSFW: boolean,
-  author: string
+  title: string;
+  edited: boolean;
+  spoiler: boolean;
+  NSFW: boolean;
+  author: string;
+
+  awards: Award[];
+}
+
+export interface Award {
+  icon: {
+    url: RedditURL;
+    format: string;
+  };
+  name: string;
 }
